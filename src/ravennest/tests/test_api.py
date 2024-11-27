a773f834e3e7fa5860e2d2fd4772fcc0bf25b7e7
Anthony Christodoulou <christodoulou75@gmail.com>
2024-11-27 21:54:29 +0100

    correceted tests

--- src/ravennest/tests/test_api.py
+++ src/ravennest/tests/test_api.py
@@ -1,10 +1,8 @@
 
 from ravennest.api.coingecko import get_current_price, get_historical_data
 
-import requests
 
-
-class test_get_current_price():
+class TestGetCurrentPrice:
 
     def test_valid_response(self, requests_mock):
         # Simuler une réponse valide de l'API
@@ -16,8 +14,8 @@ class test_get_current_price():
         price = get_current_price("bitcoin", "eur")
         assert price == 50000, "Price should be 50000 for valid response"
 
-    def test_invalid_response_structure(requests_mock):
-    # Simuler une réponse invalide de l'API
+    def test_invalid_response_structure(self, requests_mock):
+        # Simuler une réponse invalide de l'API
         mock_url = "https://api.coingecko.com/api/v3/simple/price"
         mock_response = {}  # Structure inattendue
         requests_mock.get(mock_url, json=mock_response)
@@ -26,7 +24,7 @@ class test_get_current_price():
         price = get_current_price("bitcoin", "eur")
         assert price is None, "Price should be None for invalid response structure"
 
-    def test_http_error(requests_mock):
+    def test_http_error(self, requests_mock):
         # Simuler une erreur HTTP (404 Not Found)
         mock_url = "https://api.coingecko.com/api/v3/simple/price"
         requests_mock.get(mock_url, status_code=404)
@@ -35,20 +33,9 @@ class test_get_current_price():
         price = get_current_price("bitcoin", "eur")
         assert price is None, "Price should be None for HTTP error"
 
-    def test_request_exception(mocker):
-        # Simuler une exception réseau
-        mocker.patch(
-            "requests.get",
-            side_effect=requests.exceptions.RequestException("Network error")
-            )
 
-        # Tester la fonction
-        price = get_current_price("bitcoin", "eur")
-        assert price is None, "Price should be None for network exception"
-
-
-class test_get_historical_data():
-    def get_historical_data_valid(self, requests_mock):
+class TestGetHistoricalData:
+    def test_get_historical_data_valid(self, requests_mock):
         mock_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
 
         mock_response = {"prices": [
@@ -58,5 +45,6 @@ class test_get_historical_data():
         requests_mock.get(mock_url, json=mock_response)
 
         # Tester la fonction
-        price = get_historical_data("bitcoin")
-        print(price)
+        data = get_historical_data("bitcoin")
+        assert len(data) == 2, "Expected two data points"
+        # assert data["prices"[0][1] == 69702.3087473573, "First price should match"
